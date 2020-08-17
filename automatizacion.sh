d8bb3ab815816c2da30c3fcccf989aa91bc94e3b
luis <lmoralese1401@alumno.ipn.mx>
2020-08-17 13:39:26 -0500

    en proceso

---
+++ automatizacion.sh
@@ -0,0 +1,33 @@
+echo"script automatizacion de instalacion debian 10"
+echo"actualizacion" 
+sudo apt-get update && sudo apt-get upgrade
+echo"instalando java 11"
+sudo apt install default-jre
+echo "verificando la version de java" 
+java -version
+echo "instalando el compilaodr de java en terminal" 
+sudo apt install default-jdk
+echo "verificando la version del compilador de java" 
+javac -version
+echo "instalando snap"
+sudo apt install snapd
+echo "probando snap"
+snap install hello-world
+echo "verificando snapd" 
+hello-world
+
+
+
+
+-netbeans
+sudo snap install netbeans --classic
+-vscode
+-android studio
+-virtualbox
+
+-flutter from snap
+-flutter doctor 
+-flutter licenses
+-instalar veracript
+
+

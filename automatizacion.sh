7d75f75407db9681117ed5652ae6c02b8cc72de7
luis <lmoralese1401@alumno.ipn.mx>
2020-08-31 12:53:19 -0500

    version 1.1

--- automatizacion.sh
+++ automatizacion.sh
@@ -10,7 +10,6 @@ echo "instalando el compilaodr de java en terminal"
 sudo apt install default-jdk -y
 echo "verificando la version del compilador de java" 
 javac -version
-
 echo "instalando git "
 sudo apt install git -y
 echo "arreglando las dependencias faltantes"
@@ -25,9 +24,7 @@ echo "instalando la utilidad wget para descargar android studio"
 apt-get install wget -y
 echo "descargando androdid studio desde la pagina oficial"
 sudo wget -O android-studio.tar.gz https://redirector.gvt1.com/edgedl/android/studio/ide-zips/4.0.1.0/android-studio-ide-193.6626763-linux.tar.gz
-
 echo "obteninedo vercript"
-
 wget -O veracript-1.24-update7-debian10.deb https://launchpad.net/veracrypt/trunk/1.24-update7/+download/veracrypt-1.24-Update7-Debian-10-amd64.deb
 cd /home/luis/Descargas
 ls
@@ -35,7 +32,15 @@ echo "instalando veracypt"
 sudo dpkg -i veracript-1.24-update7-debian10.deb
 echo "arreglando las dependencias faltantes"
 sudo apt-get -f install
+echo "instalando gnome boxes"
+sudo apt-get install gnome-boxes
+echo "instalndo telegram"
+sudo apt-get install telegram-desktop
+echo "resolviendo dependencias"
+sudo apt-get -f install
 
+
+echo "ahora tienes que reiniciar la computadora y ejecutar snapapps en esta misma carpeta"
 
 
 
--- snapapps.sh
+++ snapapps.sh
@@ -1,16 +1,9 @@
+#!/bin/bash
 echo "instalacion de programas desde snap"
-
-
 echo "instalanado vscode"
 sudo snap install --classic code
-
-echo "instalando virtualbox"
-sudo nano /etc/apt/sources.list.d/oracle-virtualbox.list
-
-
-
--flutter from snap
--flutter doctor 
--flutter licenses
--instalar veracript
-
+echo "instalando flutter"
+sudo snap install flutter --classic
+echo "instalando netbeans"
+sudo snap install netbeans --classic
+echo "recuerda que hace falta correr flutter doctor para las licencias"
